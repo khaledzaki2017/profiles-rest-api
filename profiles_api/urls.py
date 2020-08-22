dc3bf3cbf11ff7ec5bee26307826d77c57aed286
khaledzaki2017 <khaledzaky07@gmail.com>
2020-08-22 22:25:51 +0200

    added POST,PUT,PATCH and DELETE methods to my apiView

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -3,6 +3,6 @@ from profiles_api import views
 
 
 urlpatterns = [
-    path('hello-view/', views.HelloApiView.as_view()),
-
+    path("hello-view/", views.HelloApiView.as_view()),
+    #path('hello-view/<int:pk>/', views.HelloApiView.as_view()),
 ]
